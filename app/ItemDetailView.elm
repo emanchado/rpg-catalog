47bf35e2c2fa2e828e6260690a671fbdbbd79bde
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2016-07-12 22:28:20 +0200

    Minimal item detail page with link back to section

--- app/ItemDetailView.elm
+++ app/ItemDetailView.elm
@@ -1,15 +1,26 @@
 module ItemDetailView exposing (view)
 
 -- import Dict
-import Html exposing (Html, div, span, input, text, img)
-import Html.Attributes exposing (src, class)
+import Html exposing (Html, div, span, a, input, text, img)
+import Html.Attributes exposing (class, src, href)
 import Html.Events exposing (onClick)
 
 import Models exposing (..)
 import Actions exposing (..)
 import ViewUtils
+import ModelUtils
 
 view : CatalogData -> Int -> Html Msg
 view catalog itemId =
-  div []
-    [ text ("Show item: " ++ (toString itemId)) ]
+  case ModelUtils.getSectionItem catalog itemId of
+    Just (section, item) ->
+      div []
+        [ div [] [ text ("Show item: " ++ (toString itemId)) ]
+        , a [ href ("#section/" ++ (toString section.id))
+            , onClick (ShowSection section.id)
+            ]
+            [ text "Back to section" ]
+        ]
+    Nothing ->
+      div []
+        [ text ("Item " ++ (toString itemId) ++ " does not exist") ]
--- app/ModelUtils.elm
+++ app/ModelUtils.elm
@@ -22,3 +22,18 @@ getSectionTags section =
        (\i tags -> Set.union tags (Set.fromList i.tags))
        Set.empty
        section.items)
+
+getSectionItem : CatalogData -> Int -> Maybe (CatalogSection, CatalogItem)
+getSectionItem catalog itemId =
+  List.foldl
+    (\s res ->
+       let
+         foundItem = List.head (List.filter (\i -> i.id == itemId) s.items)
+       in
+         case foundItem of
+           Just item ->
+             Just (s, item)
+           Nothing ->
+             res)
+    Nothing
+    catalog.sections
